9b006067c43b7073f06f7cfb567e730b89baacd4
masha-svet <mashasvetvik@gmail.com>
2022-10-24 21:07:30 +0300

    класс, управляющий пулями

--- settings.py
+++ settings.py
@@ -2,5 +2,16 @@ class Settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (51, 102, 153)
         self.ship_speed = 2
+        self.ship_limit = 3
+
+        self.bullet_speed = 2
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 10
+
+        self.alien_speed = 1.5
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
--- ship.py
+++ ship.py
@@ -8,17 +8,22 @@ class Ship():
 
         self.image = pygame.image.load('../../../Desktop/project1/rocket_1.bmp')
         self.rect = self.image.get_rect()
-        self.rect.midleft = self.screen_rect.midleft
+
+        self.rect.midright = self.screen_rect.midright
         self.y = float(self.rect.y)
+        self.x = float(self.rect.x)
 
         self.moving_up = False
         self.moving_down = False
-
     def update(self):
-        if self.moving_down and self.rect.down < self.screen_rect.down:
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
             self.y += self.settings.ship_speed
-        if self.moving_up and self.rect.up>0:
+        if self.moving_up and self.rect.top > 0:
             self.y -= self.settings.ship_speed
         self.rect.y = self.y
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.rect.midright = self.screen_rect.midright
+        self.y = float(self.rect.y)
