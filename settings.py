a91dcd9e72b56fa434012e7ef29fd3ea8f916ecd
masha-svet <mashasvetvik@gmail.com>
2022-10-25 14:50:56 +0300

    добавление увеличения скорости

--- settings.py
+++ settings.py
@@ -3,15 +3,32 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (51, 102, 153)
-        self.ship_speed = 2
-        self.ship_limit = 3
 
-        self.bullet_speed = 2
+        self.ship_limit = 3
+        self.ship_speed =2
+
         self.bullet_width = 15
         self.bullet_height = 3
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 10
+        self.bullet_speed = 3
 
-        self.alien_speed = 1.5
         self.fleet_drop_speed = 10
+        self.alien_speed = 1.5
+
+        self.speedup_scale = 1.5
+        self.score_scale = 1.5
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed_factor = 2
+        self.bullet_speed_factor = 3.0
+        self.alien_speed_factor = 1.5
         self.fleet_direction = 1
+        self.alien_points = 50
+
+    def increase_speed(self):
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
